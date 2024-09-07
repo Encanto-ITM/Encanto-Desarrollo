0fbb3f906106103bd5f8b66cab4b60da3401787d
PuzzledStone <ianxd03@gmail.com>
2024-09-06 18:07:48 -0600

    Added landing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
+import Landing from './Pages/Landing';
 
 export function App() {
     return (
         <div>
             <Routes>
     
-                <Route path="/*" element={<Navigate to="/login" />} />
+                <Route path="/*" element={<Navigate to="/landing" />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/landing" element={<Landing />} />
                 
             </Routes>
         </div>
