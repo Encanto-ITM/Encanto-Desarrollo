4d3da3440a1c7462792cf2c38521c182c1d78046
PuzzledStone <ianxd03@gmail.com>
2024-09-16 16:04:49 -0600

    ServiceCard created

---
+++ src/Components/UI/ServiceCard.jsx
@@ -0,0 +1,8 @@
+export default function ServiceCard({ serviceName }) {
+    return (
+        <section className="flex flex-col items-center bg-white p-6 drop-shadow-md rounded-xl gap-4">
+            <img className="h-auto object-contain" src="/img/Barberia.png" alt="service" />
+            <h2 className="text-purple text-center">{serviceName}</h2>
+        </section>
+    );
+}
