d3300a88e895842407eb0b64756136f1180daec4
PuzzledStone <ianxd03@gmail.com>
2024-10-29 03:59:22 -0600

    animation added to modals

--- src/Components/UI/EditProfile.jsx
+++ src/Components/UI/EditProfile.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Modal from '@mui/material/Modal';
 import ImageUploader from './ImageUploader';
 import EditInput from './EditInputs';
+import Box from '@mui/material/Box';
+import Grow from '@mui/material/Grow';
 
 export default function EditProfile({ open, onClose, user, onProfileUpdated }) {
   const [name, setName] = useState('');
@@ -11,17 +13,23 @@ export default function EditProfile({ open, onClose, user, onProfileUpdated }) {
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+  const [isExiting, setIsExiting] = useState(false);
 
   useEffect(() => {
     if (open) {
-      setName('');
-      setLastname('');
-      setEmail('');
-      setDescription('');
-      setOldPassword('');
-      setNewPassword('');
+      setShowContent(true);
+      setIsExiting(false);
+      setName(user.name || '');
+      setLastname(user.lastname || '');
+      setEmail(user.email || '');
+      setDescription(user.description || '');
+    } else {
+      if (isExiting) {
+        setShowContent(false);
+      }
     }
-  }, [open]);
+  }, [open, user, isExiting]);
 
   const handleImageChange = (image) => {
     setSelectedImage(image); 
@@ -39,20 +47,10 @@ export default function EditProfile({ open, onClose, user, onProfileUpdated }) {
       return;
     }
 
-    const data = {
-      name,
-      lastname,
-      email,
-      description,
-    };
-
-    if (newPassword) {
-      data.password = newPassword;
-    }
+    const data = { name, lastname, email, description, ...(newPassword && { password: newPassword }) };
 
     try {
       setIsLoading(true);
-
       const response = await fetch(`https://tulookapiv2.vercel.app/api/api/users/${user.id}`, {
         method: 'PUT',
         headers: {
@@ -64,15 +62,12 @@ export default function EditProfile({ open, onClose, user, onProfileUpdated }) {
 
       if (!response.ok) {
         const errorMessage = await response.text();
-        console.error('Error en la respuesta de la API:', errorMessage);
         throw new Error(`Error al actualizar la información: ${errorMessage}`);
       }
 
       const responseBody = await response.json();
-      console.log('Perfil actualizado desde la API:', responseBody);
-
       onProfileUpdated(responseBody);
-      onClose();
+      closeModal();
     } catch (error) {
       console.error('Error al actualizar la información:', error);
     } finally {
@@ -80,75 +75,53 @@ export default function EditProfile({ open, onClose, user, onProfileUpdated }) {
     }
   };
 
+  const closeModal = () => {
+    setIsExiting(true);
+    setTimeout(() => {
+      onClose();
+    }, 500); 
+  };
+
   return (
-    <Modal open={open} onClose={onClose}>
-      <div className="fixed inset-0 flex items-start justify-end p-4 pt-20">
-        <div className="max-w-lg w-full rounded-lg shadow-lg overflow-y-auto" style={{ maxHeight: '90vh' }}>
-          
-          <div className="bg-white text-black rounded-t-lg p-4"> {/* Añadido padding */}
-            <button onClick={onClose} className="absolute top-[5.5rem] right-8 text-black">X</button>
-            <h2 className="text-2xl font-bold text-center mb-4">Editar Perfil</h2>
-            <ImageUploader onImageChange={handleImageChange} />
-          </div>
-          
-          <div className="bg-purple text-white p-6 rounded-b-lg">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4 justify-items-center">
-              <EditInput 
-                label="Nombre" 
-                id="name" 
-                value={name} 
-                onChange={(e) => setName(e.target.value)} 
-              />
-              <EditInput 
-                label="Apellido" 
-                id="lastname" 
-                value={lastname} 
-                onChange={(e) => setLastname(e.target.value)} 
-              />
-              <EditInput 
-                label="Correo Electrónico" 
-                id="email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-              />
-              <EditInput 
-                label="Descripción" 
-                id="description" 
-                value={description} 
-                onChange={(e) => setDescription(e.target.value)} 
-              />
-              <EditInput 
-                label="Contraseña Vieja" 
-                id="oldPassword" 
-                type="password" 
-                value={oldPassword} 
-                onChange={(e) => setOldPassword(e.target.value)} 
-              />
-              <EditInput 
-                label="Nueva Contraseña" 
-                id="newPassword" 
-                type="password" 
-                value={newPassword} 
-                onChange={(e) => setNewPassword(e.target.value)} 
-              />
-            </div>
-  
-            <div className="flex justify-center mt-6">
+    <Modal open={open} onClose={closeModal}>
+      <Box className="fixed inset-0 flex items-start justify-end p-4 pt-20">
+        <Grow in={showContent && !isExiting} timeout={500}>
+          <div className="max-w-lg w-full rounded-lg shadow-lg overflow-y-auto" style={{ maxHeight: '90vh' }}>
+            <div className="bg-white text-black rounded-t-lg p-4 relative">
               <button 
-                onClick={handleUpdate}
-                className={`rounded mt-4 border-2 bg-blue border-blue text-white p-2 w-3/4 mb-8 hover:scale-105 duration-500 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
-                disabled={isLoading} 
+                onClick={closeModal} 
+                className="absolute top-4 right-4 text-black focus:outline-none"
               >
-                {isLoading ? 'Actualizando...' : 'Editar'}
+                X
               </button>
+              <h2 className="text-2xl font-bold text-center mb-4  ">Editar Perfil</h2> 
+              <ImageUploader onImageChange={handleImageChange} />
+            </div>
+            
+            <div className="bg-purple text-white p-4 rounded-b-lg"> 
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6 justify-items-center">
+                <EditInput label="Nombre" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                <EditInput label="Apellido" id="lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+                <EditInput label="Correo Electrónico" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <EditInput label="Descripción" id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                <EditInput label="Contraseña Vieja" id="oldPassword" type="password" value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} />
+                <EditInput label="Nueva Contraseña" id="newPassword" type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+              </div>
+  
+              <div className="flex justify-center mt-12">
+                <button 
+                  onClick={handleUpdate}
+                  className={`rounded mt-2 border-2 bg-blue border-blue text-white p-2 w-3/4 my-8 hover:scale-105 duration-500 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                  disabled={isLoading} 
+                >
+                  {isLoading ? 'Actualizando...' : 'Editar'}
+                </button>
+              </div>
             </div>
           </div>
-          
-        </div>
-      </div>
+        </Grow>
+      </Box>
     </Modal>
   );
-  
-  
   
 }
