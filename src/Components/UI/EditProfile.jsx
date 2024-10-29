5ac86b4f9969865d2c03a7b193d4a8f210d2a980
PuzzledStone <ianxd03@gmail.com>
2024-10-29 00:55:55 -0600

    inputs centered

--- src/Components/UI/EditProfile.jsx
+++ src/Components/UI/EditProfile.jsx
@@ -83,65 +83,74 @@ export default function EditProfile({ open, onClose, user, onProfileUpdated }) {
   return (
     <Modal open={open} onClose={onClose}>
       <div className="fixed inset-0 flex items-start justify-end p-4 pt-20">
-        <div className="bg-purple text-white max-w-lg w-full rounded-lg p-6 relative shadow-lg overflow-y-auto" style={{ maxHeight: '90vh' }}>
-          <button onClick={onClose} className="absolute top-4 right-4 text-white text-lg">X</button>
-          <h2 className="text-2xl font-bold text-center mb-4">Editar Perfil</h2>
-          <ImageUploader onImageChange={handleImageChange} />
+        <div className="max-w-lg w-full rounded-lg shadow-lg overflow-y-auto" style={{ maxHeight: '90vh' }}>
           
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4 text-black justify-items-center">
-            <EditInput 
-              label="Nombre" 
-              id="name" 
-              value={name} 
-              onChange={(e) => setName(e.target.value)} 
-            />
-            <EditInput 
-              label="Apellido" 
-              id="lastname" 
-              value={lastname} 
-              onChange={(e) => setLastname(e.target.value)} 
-            />
-            <EditInput 
-              label="Correo Electrónico" 
-              id="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
-            />
-            <EditInput 
-              label="Descripción" 
-              id="description" 
-              value={description} 
-              onChange={(e) => setDescription(e.target.value)} 
-            />
-            <EditInput 
-              label="Contraseña Vieja" 
-              id="oldPassword" 
-              type="password" 
-              value={oldPassword} 
-              onChange={(e) => setOldPassword(e.target.value)} 
-            />
-            <EditInput 
-              label="Nueva Contraseña" 
-              id="newPassword" 
-              type="password" 
-              value={newPassword} 
-              onChange={(e) => setNewPassword(e.target.value)} 
-            />
+        
+          <div className="bg-white text-black  rounded-t-lg">
+            <button onClick={onClose} className="absolute top-[5.5rem] right-8 text-black">X</button>
+            <h2 className="text-2xl font-bold text-center mb-4">Editar Perfil</h2>
+            <ImageUploader onImageChange={handleImageChange} />
           </div>
-  
-          <div className="flex justify-center mt-6">
-            <button 
-              onClick={handleUpdate}
-              className={`rounded mt-4 border-2 border-blue text-blue p-2 w-3/4 mb-8 hover:scale-105 duration-500 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
-              disabled={isLoading} 
-            >
-              {isLoading ? 'Actualizando...' : 'Editar'}
-            </button>
+          
+     
+          <div className="bg-purple text-white p-6 rounded-b-lg">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4 justify-items-center">
+              <EditInput 
+                label="Nombre" 
+                id="name" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} 
+              />
+              <EditInput 
+                label="Apellido" 
+                id="lastname" 
+                value={lastname} 
+                onChange={(e) => setLastname(e.target.value)} 
+              />
+              <EditInput 
+                label="Correo Electrónico" 
+                id="email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+              />
+              <EditInput 
+                label="Descripción" 
+                id="description" 
+                value={description} 
+                onChange={(e) => setDescription(e.target.value)} 
+              />
+              <EditInput 
+                label="Contraseña Vieja" 
+                id="oldPassword" 
+                type="password" 
+                value={oldPassword} 
+                onChange={(e) => setOldPassword(e.target.value)} 
+              />
+              <EditInput 
+                label="Nueva Contraseña" 
+                id="newPassword" 
+                type="password" 
+                value={newPassword} 
+                onChange={(e) => setNewPassword(e.target.value)} 
+              />
+            </div>
+    
+            <div className="flex justify-center mt-6">
+              <button 
+                onClick={handleUpdate}
+                className={`rounded mt-4 border-2 border-blue text-blue p-2 w-3/4 mb-8 hover:scale-105 duration-500 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                disabled={isLoading} 
+              >
+                {isLoading ? 'Actualizando...' : 'Editar'}
+              </button>
+            </div>
           </div>
+          
         </div>
       </div>
     </Modal>
   );
+  
   
   
 }
